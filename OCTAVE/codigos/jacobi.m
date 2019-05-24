ff141f1712010f6fd4bacb96777f244e8c4159b9
pedrodsc <pedro.custodio@aluno.ufes.br>
2019-05-24 00:31:49 -0300

    Funçoes jacobi e sor modificadas

--- OCTAVE/codigos/jacobi.m
+++ OCTAVE/codigos/jacobi.m
@@ -1,4 +1,3 @@
-
 function [x,iter,res] = jacobi(A,b,tol,nmaxiter)
 %
 % Entrada: 
@@ -22,17 +21,7 @@ function [x,iter,res] = jacobi(A,b,tol,nmaxiter)
     err = tol+1.0;  
     x0 = zeros(n,1);
     x = x0;
-    while ((err > tol)&&(iter < nmaxiter))
-##      for i=1:n
-##        soma = 0.0;
-##        for j = 1:(i-1)
-##          soma = soma + A(i,j)*x0(j);
-##        endfor
-##        for j = (i+1):n
-##          soma = soma + A(i,j)*x0(j);
-##        endfor
-##        x(i) = (b(i) - soma)/A(i,i);
-##      endfor          
+    while ((err > tol)&&(iter < nmaxiter))     
       x = D1B - D1LU*x0;
       iter = iter + 1;
       res(iter) = norm((b-A*x)/b,inf);  
