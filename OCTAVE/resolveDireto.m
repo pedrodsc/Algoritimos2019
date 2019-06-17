5ee37eff285573039753a33d6120c276684f3740
pedrodsc <pedro.custodio@aluno.ufes.br>
2019-06-17 15:46:22 -0300

    :)

--- OCTAVE/resolveDireto.m
+++
@@ -1,9 +0,0 @@
-function [elapsedTime,normA,x] = resolveDireto (A)
-
-  n = rows(A);
-  b = A*ones(n,1);
-  t0 = clock();
-  x = A\b;
-  elapsedTime = etime(clock(),t0);
-  normA = norm((b - A*x)/b,inf)
-endfunction
